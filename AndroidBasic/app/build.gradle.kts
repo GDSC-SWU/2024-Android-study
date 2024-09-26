6003e5390219b9e7a6e4b135af50470ae4ba295d
leastzero <csong7177@naver.com>
2024-09-26 19:35:54 +0900

    [feat] gradle file update

--- AndroidBasic/app/build.gradle.kts
+++ AndroidBasic/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.androidbasic"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.androidbasic"
