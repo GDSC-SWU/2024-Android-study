edcbc37609ebfb3a0fec1f134e73c3a6628c7449
leastzero <csong7177@naver.com>
2024-11-04 21:52:02 +0900

    [feat] StopWatch (feat.Thread)

--- StopWatch/app/src/main/java/com/example/stopwatch/UpgradeMainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/UpgradeMainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import java.util.Timer
 import kotlin.concurrent.timer
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class UpgradeMainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btn_start : Button
     private lateinit var btn_refresh : Button
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_upgrade)
 
         btn_start = findViewById(R.id.btn_start)
         btn_refresh = findViewById(R.id.btn_refresh)
@@ -51,8 +51,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun start() {
-        btn_start.text = getString(R.string.btn_pause)
-        btn_start.setBackgroundColor(getColor(R.color.btn_pause))
+        btn_start.text = getString(R.string.btn_pause_eng)
         isRunning = true
 
         timer = timer(period = 10) {
@@ -75,8 +74,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun pause() {
-        btn_start.text = getString(R.string.btn_start)
-        btn_start.setBackgroundColor(getColor(R.color.btn_start))
+        btn_start.text = getString(R.string.btn_start_eng)
         isRunning = false
 
         timer?.cancel()
@@ -85,8 +83,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun refresh() {
         timer?.cancel()
 
-        btn_start.text = getString(R.string.btn_start)
-        btn_start.setBackgroundColor(getColor(R.color.btn_start))
+        btn_start.text = getString(R.string.btn_start_eng)
         isRunning = false
 
         time = 0
