78ed3f7afdc8acc65187cf44f0c0cf5ebde7e877
Riny <add6847@naver.com>
2024-11-21 00:43:55 +0900

    week7-seorin

--- CloneCoding/app/build.gradle.kts
+++ CloneCoding/app/build.gradle.kts
@@ -1,19 +1,28 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
+val localPropertiesFile = rootProject.file("local.properties")
+val properties = Properties()
+properties.load(FileInputStream(localPropertiesFile))
+
 android {
-    namespace = "com.example.clonecoding"
-    compileSdk = 33
+    namespace = "com.rinylecture.AirQuality"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.clonecoding"
-        minSdk = 24
+        applicationId = "com.rinylecture.AirQuality"
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
+        buildConfigField("String", "API_KEY", "\"${properties["airqualityapi.key"]}\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -33,6 +42,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -44,4 +58,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
